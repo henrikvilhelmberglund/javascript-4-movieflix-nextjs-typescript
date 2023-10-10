4ad38f4ada26dc347c453447732ba83f86cbb89b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-10 10:33:50 +0200

    feat: :white_check_mark: add new test

--- __test__/pages/index.test.tsx
+++ __test__/pages/index.test.tsx
@@ -1,11 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Home from "@/pages";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 
 describe("homepage", () => {
-  it("has a text string ", () => {
+  it("has a heading ", () => {
     render(<Home />);
-    const textToTest = screen.getByText("pages/index.tsx");
-    expect(textToTest).toBeInTheDocument()
+    const heading = screen.getByRole("heading", {
+      name: /välkommen till movieflix/i,
+      level: 1
+    });
+    expect(heading).toBeInTheDocument();
   });
+  it("has a introductary text string", () => {
+    render(<Home />);
+    const text = screen.getByText(/vi erbjuder de senaste filmerna och tv-serierna/i)
+    expect(text).toBeInTheDocument();
+  } )
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-     
+      <section>
+        <h1>Välkommen till MovieFlix!</h1>
+        <p>Vi erbjuder de senaste filmerna och TV-serierna</p>
+      </section>
     </>
-  )
+  );
 }
