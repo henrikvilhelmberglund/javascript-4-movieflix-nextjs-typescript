52de31909c92b2a51a8257ae915dc1f1212a566a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-11 15:35:55 +0200

    feat: :white_check_mark: update tests

--- __test__/pages/movies/index.test.tsx
+++ __test__/pages/movies/index.test.tsx
@@ -1,11 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import MoviesPage from "@/pages/movies";
+import { getAllMovies } from "@/mock/data/movies";
+
+let movies: Movie[] = [];
 
 describe("movies page", () => {
+  beforeEach(() => {
+   movies = getAllMovies();
+    render(<MoviesPage movies={movies} />);
+    return movies;
+  })
+  
   it("has a heading", () => {
-    render(<MoviesPage />);
     const heading = screen.getByRole("heading", { name: /populära filmer/i });
     expect(heading).toBeInTheDocument();
+  });
+
+  it("has a list of 20 movies", () => {
+    expect(movies).toHaveLength(20);
+  });
+
+  it("has a poster", () => {
+    const poster = screen.getByRole("img", {name: movies[0].title})
+    expect(poster).toBeInTheDocument();
   });
 });
