d4c18d4e2078fbd25149a3aa933f6d616f0f08d2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-11 17:36:12 +0200

    feat: :white_check_mark: add more tests

--- __test__/pages/tv-series/index.test.tsx
+++ __test__/pages/tv-series/index.test.tsx
@@ -3,14 +3,26 @@ import "@testing-library/jest-dom";
 import TVSeriesPage from "@/pages/tv-series";
 import { getAllTVSeries } from "@/mock/data/tv";
 
+let series: Serie[];
+
 describe("tv series page", () => {
   beforeEach(() => {
-    const series = getAllTVSeries();
+    series = getAllTVSeries();
     render(<TVSeriesPage series={series} />);
+    return series;
   });
 
   it("has a heading", () => {
     const heading = screen.getByRole("heading", { name: /tv series/i });
     expect(heading).toBeInTheDocument();
+  });
+
+  it("has a list of 20 tv series", () => {
+    expect(series).toHaveLength(20);
+  });
+
+  it("has a poster", () => {
+    const poster = screen.getByRole("img", { name: series[0].title });
+    expect(poster).toBeInTheDocument();
   });
 });
