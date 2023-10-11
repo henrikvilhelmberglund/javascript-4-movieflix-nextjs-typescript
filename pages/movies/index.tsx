9eee8778945a5f590d0c17c9f173c4bc3981f3a3
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-11 15:37:33 +0200

    feat: :sparkles: add props and loop through movies

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,7 +1,13 @@
-export default function MoviesPage() {
+export default function MoviesPage({ movies }: Movies) {
   return (
     <>
       <h1>Populära filmer</h1>
+      {movies.map((movie) => (
+        <article key={movie.id}>
+          <h2>{movie.title}</h2>
+          <img src={movie.poster} width="400" alt={movie.title} />
+        </article>
+      ))}
     </>
   );
 }
